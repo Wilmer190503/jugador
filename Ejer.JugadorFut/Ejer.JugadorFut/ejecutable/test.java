de07f3275947b5dd92787a1a03c8046d85511d0d
Wilmer Andrés Romero Cala <103191577+Wilmer190503@users.noreply.github.com>
2022-06-23 07:39:07 -0500

    Update test.java

--- Ejer.JugadorFut/Ejer.JugadorFut/ejecutable/test.java
+++ Ejer.JugadorFut/Ejer.JugadorFut/ejecutable/test.java
@@ -6,8 +6,8 @@ import modelo.Jugador;
 public class test {
     public static void main(String[] args)
     {
-        Equipo Equipo1 = new Equipo("sergio",12);
-        Jugador Jugador1 = new Jugador("Crisitano", 13, 18);
+        Equipo Equipo1 = new Equipo("Andrés",19);
+        Jugador Jugador1 = new Jugador("Messi", 23, 10);
         Equipo1.crearJugador();
         System.out.println(Equipo1.toString());
     }
